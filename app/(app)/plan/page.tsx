7e6ec815e3e3a44ce2350caf69f1a7e507d381be
Eze Justin <ezejustin792@gmail.com>
2026-03-29 19:03:24 -0700

    Fix Suspense import on plan page

--- app/(app)/plan/page.tsx
+++ app/(app)/plan/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState, Suspense } from 'react'
 import { useRouter, useSearchParams } from 'next/navigation'
 import ReactMarkdown from 'react-markdown'
 import { Lock } from 'lucide-react'
